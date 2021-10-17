48a39745b3d0ccb905f963593ab6c702cae7effe
tunogya <tunogya@qq.com>
2021-10-17 19:22:23 +0800

    update

--- contracts/EIP20Factory.sol
+++ contracts/EIP20Factory.sol
@@ -11,7 +11,7 @@ contract EIP20Factory {
 
     function EIP20Factory() public {
         //upon creation of the factory, deploy a EIP20 (parameters are meaningless) and store the bytecode provably.
-        address verifiedToken = createEIP20(100000000000000000000000000, "Gomboc", 2, "GMC");
+        address verifiedToken = createEIP20(100000000000000000000000000, "Gomboc", 18, "GMC");
         EIP20ByteCode = codeAt(verifiedToken);
     }
 
