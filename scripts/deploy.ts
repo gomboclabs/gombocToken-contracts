f55e9b5e7bbaea71000407b07d9b48c3c8df38a0
tunogya <tunogya@qq.com>
2021-10-26 17:09:05 +0800

    update deploy.ts

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,29 +1,18 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
 import { ethers } from "hardhat";
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+  const GombocToken = await ethers.getContractFactory("GombocToken");
+  const gombocToken = await GombocToken.deploy();
+  await gombocToken.deployed();
+  console.log("GombocToken deployed to:", gombocToken.address);
 
-  // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const GombocNFT = await ethers.getContractFactory("GambocNFT");
+  const gombocNFT = await GombocNFT.deploy();
+  await gombocNFT.deployed();
 
-  await greeter.deployed();
-
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("GombocNFT deployed to:", gombocNFT.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
